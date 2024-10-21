5bc2c7ec8db38b5a4cfc8cdafab1f3238bab90c5
Andrey Arama <anarama@c3r4p6.42vienna.com>
2024-10-21 20:13:48 +0200

    ex00 probably done

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,20 +6,32 @@
 /*   By: anarama <anarama@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 14:06:51 by anarama           #+#    #+#             */
-/*   Updated: 2024/10/21 14:56:00 by anarama          ###   ########.fr       */
+/*   Updated: 2024/10/21 20:12:11 by anarama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+# include <exception>
+# include <algorithm>
 
-class OutOfRangeException(): public 
+class OutOfRangeException: public std::exception {
+	public:
+		const char* what() const throw() {
+			return "Accesing entry out of range!";
+		}
+};
 
 template <typename T>
-typename T::iterator easyfind( T data, int n ) {
+typename T::iterator easyfind( T& data, int n ) {
 	typename T::iterator it;
-	
+	it = std::find(data.begin(), data.end(), n);
+	if (it != data.end()) {
+		return it;
+	} else {
+		throw OutOfRangeException();
+	}
 }
 
 #endif // EASYFIND_HPP
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,10 +6,34 @@
 /*   By: anarama <anarama@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 14:06:53 by anarama           #+#    #+#             */
-/*   Updated: 2024/10/19 14:07:18 by anarama          ###   ########.fr       */
+/*   Updated: 2024/10/21 20:13:03 by anarama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "easyfind.hpp"
+#include <vector>
+
 int main (void) {
+	int arr[] = {5, 3, 2, 9, 1};
+	std::vector<int> list(arr, arr + sizeof(arr) / sizeof(arr[0]));
+	
+	for (size_t i = 0; i < list.size(); ++i) {
+        std::cout << list[i] << " ";
+    }
+    std::cout << std::endl;
+	
+	try {
+		std::vector<int>::iterator it = easyfind(list, 2);
+		std::cout << "Value found: " << *it << std::endl;
+	} catch (const OutOfRangeException& e) {
+		std::cout << "Value was not found!" << std::endl;
+	}
+	try {
+		std::vector<int>::iterator it = easyfind(list, 10);
+		std::cout << "Value found: " << *it << std::endl;
+	} catch (const OutOfRangeException& e) {
+		std::cout << "Value was not found!" << std::endl;
+	}
 	return 0;
 }
